98a38334af511642f2255ad3c7aba9a9990bc607
Nguyễn  Thái <thainh@twinger.vn>
2023-04-23 15:32:02 +0700

    init swiper

---
+++ src/components/widgets/SlideShowCustom/index.jsx
@@ -0,0 +1,83 @@
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { Button, Col, Empty, Row } from 'antd';
+import React from 'react';
+import { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import styles from './style.module.scss';
+
+function SlideShowCustom({
+  isPagination = false,
+  isNavigation = false,
+  isPaginationAbsolute = false,
+  slideElement,
+  slideKey,
+  customPadding,
+  slideHeight = 200,
+  ...props
+}) {
+  const top = isNavigation && slideHeight ? slideHeight / 2 : '50%';
+  return (
+    <Row gutter={[0, 30]} className={styles.wrapper}>
+      {slideElement && slideElement?.length > 0 ? (
+        <>
+          <Col span={24} className={styles.swiper}>
+            <Swiper
+              modules={[Navigation, Pagination]}
+              spaceBetween={0}
+              slidesPerView={1}
+              className={customPadding ? styles.slide : ''}
+              navigation={
+                isNavigation
+                  ? {
+                      nextEl: `.button-next-${slideKey}`,
+                      prevEl: `.button-prev-${slideKey}`,
+                    }
+                  : false
+              }
+              pagination={isPagination ? { clickable: true, el: `.pagination-${slideKey}` } : false}
+              {...props}
+            >
+              {slideElement?.map((e, index) => (
+                <SwiperSlide key={index}>{e}</SwiperSlide>
+              ))}
+            </Swiper>
+            {isNavigation && (
+              <>
+                <Button
+                  style={{ top }}
+                  className={`tw-button-navigation ${styles.navigation} ${styles.navigationPrev} button-prev-${slideKey}`}
+                  shape='circle'
+                >
+                  <LeftOutlined className={styles.iconNavigation} />
+                </Button>
+                <Button
+                  style={{ top }}
+                  className={`tw-button-navigation ${styles.navigation} ${styles.navigationNext} button-next-${slideKey}`}
+                  shape='circle'
+                >
+                  <RightOutlined className={styles.iconNavigation} />
+                </Button>
+              </>
+            )}
+          </Col>
+          {isPagination && (
+            // eslint-disable-next-line react/self-closing-comp
+            <Col
+              span={24}
+              className={`tw-pagination ${styles.pagination} ${
+                isPaginationAbsolute ? styles.paginationAbsolute : ''
+              } pagination-${slideKey}`}
+            ></Col>
+          )}
+        </>
+      ) : (
+        <Col span={24}>
+          <Empty description='Không có dữ liệu' image={Empty.PRESENTED_IMAGE_SIMPLE} />
+        </Col>
+      )}
+    </Row>
+  );
+}
+
+export default SlideShowCustom;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useMemo } from 'react';
+import SlideShowCustom from '@components/widgets/SlideShowCustom';
 
 export default function Home() {
-  return <div>Home</div>;
+  const slideElement = useMemo(
+    () =>
+      [1, 2, 3, 4, 5, 6, 7, 8].map((e, i) => (
+        <div key={i} style={{ height: 200, background: 'rgb(54, 77, 121)' }}>
+          {e}
+        </div>
+      )),
+    []
+  );
+  return (
+    <div>
+      <SlideShowCustom
+        slideElement={slideElement}
+        isNavigation
+        isPagination={false}
+        slidesPerView={4}
+        spaceBetween={40}
+        slideKey='slide-news-with-thumbnail'
+        // isPaginationAbsolute
+      />
+    </div>
+  );
 }
